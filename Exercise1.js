b1a0c1247a00edbe5df496439b56809878083d2d
zoe-c <zoecooper@MR01_Blackwell_ATC.car01.drhm.nc.frontiernet.net>
2017-02-28 21:08:52 -0500

    Exercise 1 complete

---
+++ Exercise1.js
@@ -0,0 +1,16 @@
+// function addNumbers(numberA, numberB) {
+//   console.log(numberA + numberB);
+// }
+//
+// var twoPlusTwo = addNumbers(2, 2)
+//
+// console.log(twoPlusTwo) // 4
+
+
+function addNumbers(numberA, numberB) {
+  return numberA + numberB;
+}
+
+var twoPlusTwo = addNumbers(2, 2)
+
+console.log(twoPlusTwo);
