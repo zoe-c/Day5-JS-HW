86a20d4ed86f29ed18b15bc6acec595cf05b7798
zoe-c <zoecooper@Zoe-Coopers-MacBook-Pro.local>
2017-03-01 14:35:32 -0500

    ATTEMPTS// EXERCISE 4
    -did not complete, but tried.
    -just seem to be getting tangled up in my bad code/ multiple
    attempts…going to accept defeat.

---
+++ Exercise4.js
@@ -0,0 +1,62 @@
+// Write a function called isVowel that takes a character
+// (i.e. a string of length 1) and returns true if it is a vowel, uppercase or lowercase.
+// The function should return false if the character is not a vowel.
+
+function isVowel(x){
+  var x = [];
+  var vowel = ["A","a","E","e","I","i","O","o","U,""u"];
+  if (x[0] === vowel [0,1,3,4,5,6,7,8,9]){ return true;
+  }else {return false;}
+}
+
+console.log(isVowel(A));
+
+
+
+// ANOTHER
+function isVowel("letter"){
+  var vowel = ["A","a","E","e","I","i","O","o","U,""u"];
+  if ("letter" === vowel[0,1,2,3,4,5,6,7,8,9]){ console.log("true");
+  }else {console.log ("false");}
+}
+
+console.log(isVowel("A"));
+
+
+// ANOTHER
+function isVowel(letter){
+  var letter = [letter];
+  var vowel = ["A","a","E","e","I","i","O","o","U,""u"];
+  if (letter[0] === vowel [0,1,3,4,5,6,7,8,9];){ console.log("true");
+}else if {console.log("false");}
+}
+
+console.log(isVowel("A"));
+
+
+
+// ANOTHER
+ function isVowel(letter){
+  var vowel = ["A","a","E","e","I","i","O","o","U,""u"];
+  if (letter === vowel){ console.log("true");
+}else if {console.log("false");}
+}
+
+console.log(isVowel("A"));
+
+
+
+
+// ANOTHER
+
+var vowelTrue = true;
+var vowelFalse= false;
+var vowel = ["A","a","E","e","I","i","O","o","U,""u"];
+
+function isVowel("x") {
+  if ("x" === vowel[0,1,2,3,4,5,6,7,8,9]){
+    return vowelTrue;
+  }else {return vowelFalse;}
+}
+
+console.log(isVowel("A"));
